e110be08a90e4c63ec87fc204655d69897fbd050
Nanar1 <koly.bessonov.2004@mail.ru>
2023-06-01 13:03:15 +0700

    Work_events

--- EventsDump.py
+++ EventsDump.py
@@ -30,7 +30,7 @@ async def get_course(message: types.Message, state: FSMContext):
         count = cat
         page = 0
         markup = types.InlineKeyboardMarkup()
-        markup.add(types.InlineKeyboardButton(text='Записаться', callback_data='unseen'))
+        markup.add(types.InlineKeyboardButton(text='Записаться',callback_data="{\"method\":\"unseen\",\"NumberPage\":" + str(page) + ",\"CountPage\":" + str(count) +"}"))
         markup.add(types.InlineKeyboardButton(text=f'{page + 1}/{count}', callback_data=f' '),
                    types.InlineKeyboardButton(text=f'Вперёд --->',
                                               callback_data="{\"method\":\"pagination\",\"NumberPage\":" + str(
@@ -65,41 +65,23 @@ async def pagination_func(callback: types.CallbackQuery, n=-1):
         req = callback.data.split('_')
 
         # Обработка кнопки - скрыть
-        if req[0] == 'unseen':
+        if 'unseen' in req[0]:
 
+            json_string = json.loads(req[0])
+            count = json_string['CountPage']
+            page = json_string['NumberPage']
             markup = types.InlineKeyboardMarkup()
             markup.add(types.InlineKeyboardButton(text='Записаться', callback_data='confirm_on_event'))
-            # await callback.message.edit_media(
-            #     media=aiogram.types.InputMedia(type='photo', media=open(i[page]['image']['src'], 'rb'),
-            #     caption=f"Вы хотите записаться на:\n{i[page]['name']}\n\n" +
-            #    f"Целевая аудитория: {i[page]['targetGroup']['name']}\n\n" +
-            #    f"Тип курса: {i[page]['type']['name']}\n\n" +
-            #    f"Почта: {i[page]['speakerEmail']}\n\n" +
-            #    f"Телефон для связи: {i[page]['speakerPhone']}\n\n" +
-            #    f"Начало: {i[page]['begin'][3]}:{i[page]['begin'][4]}  Дата:{i[page]['begin'][2]}.{i[page]['begin'][1]}.{i[page]['begin'][0]}"+
-            #    f"\n‼️Внимание, при записи на курс, вы даёте согласие на обработку персональных данных, которые были записаны при регистрации",
-            # ), reply_markup=markup)
-            await callback.message.answer("Вы точно хотите записаться?", reply_markup=markup)
-            print(req[0])
-        req = callback.data.split('_')
-        # Обработка кнопки - скрыть
-        i = categories['content']
-
-        # Расспарсим полученный JSON
-        json_string = json.loads(req[0])
-        count = json_string['CountPage']
-        page = json_string['NumberPage']
-        if req[0] == 'unseen':
-            markup = types.InlineKeyboardMarkup()
-            markup.add(types.InlineKeyboardButton(text='Записаться', callback_data='confirm_on_event'))
-            text = f"Вы хотите записаться на:\n {i[page]['name']}\n\n"
-            f"Целевая аудитория: {i[page]['targetGroup']['name']}\n\n"
-            f"Тип курса: {i[page]['type']['name']}\n\n"
-            f"Почта: {i[page]['speakerEmail']}\n\n"
-            f"Телефон для связи: {i[page]['speakerPhone']}\n\n"
-            f"Начало: {i[page]['begin'][3]}:{i[page]['begin'][4]}  Дата:{i[page]['begin'][2]}.{i[page]['begin'][1]}.{i[page]['begin'][0]}"
-            "\n‼️Внимание, при записи на курс, вы даёте согласие на обработку персональных данных, которые были записаны при регистрации"
-            await callback.message.edit_text(text=text, reply_markup=markup)
+            await callback.message.edit_media(
+                media=aiogram.types.InputMedia(type='photo', media=open(i[page]['image']['src'], 'rb'),
+                caption=f"Вы хотите записаться на:\n{i[page]['name']}\n\n" +
+               f"Целевая аудитория: {i[page]['targetGroup']['name']}\n\n" +
+               f"Тип курса: {i[page]['type']['name']}\n\n" +
+               f"Почта: {i[page]['speakerEmail']}\n\n" +
+               f"Телефон для связи: {i[page]['speakerPhone']}\n\n" +
+               f"Начало: {i[page]['begin'][3]}:{i[page]['begin'][4]}  Дата:{i[page]['begin'][2]}.{i[page]['begin'][1]}.{i[page]['begin'][0]}"+
+               f"\n\n‼️Внимание, при записи на курс, вы даёте согласие на обработку персональных данных, которые были записаны при регистрации",
+            ), reply_markup=markup)
 
     # Обработка кнопок - вперед и назад
         elif 'pagination' in req[0]:
@@ -111,7 +93,8 @@ async def pagination_func(callback: types.CallbackQuery, n=-1):
 
             # Пересоздаем markup
             markup = types.InlineKeyboardMarkup()
-            markup.add(types.InlineKeyboardButton(text='Записаться', callback_data='unseen'))
+            markup.add(types.InlineKeyboardButton(text='Записаться', callback_data="{\"method\":\"unseen\",\"NumberPage\":" + str(
+                                                    page) + ",\"CountPage\":" + str(count) +"}"))
             # markup для первой страницы
 
             #photo = open('C:/Users/12/PycharmProjects/Telegram_Bot_Copp_Aiogran/photos/ej.jpg', 'rb')
