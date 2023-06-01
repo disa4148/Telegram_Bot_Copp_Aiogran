3361f40822965f92ebfab27436ff680004721c81
Nanar1 <koly.bessonov.2004@mail.ru>
2023-06-01 10:55:18 +0700

    events_work

--- EventsDump.py
+++ EventsDump.py
@@ -26,7 +26,7 @@ async def get_course (message: types.Message, state: FSMContext):
     count = cat
     page = 0
     markup = types.InlineKeyboardMarkup()
-    markup.add(types.InlineKeyboardButton(text='Скрыть', callback_data='unseen'))
+    markup.add(types.InlineKeyboardButton(text='Записаться', callback_data='unseen'))
     markup.add(types.InlineKeyboardButton(text=f'{page + 1}/{count}', callback_data=f' '),
                types.InlineKeyboardButton(text=f'Вперёд --->',
                                           callback_data="{\"method\":\"pagination\",\"NumberPage\":" + str(
@@ -59,21 +59,30 @@ async def pagination_func(callback: types.CallbackQuery, n=-1):
 
     req = callback.data.split('_')
     # Обработка кнопки - скрыть
+    i = categories['content']
+
+    # Расспарсим полученный JSON
+    json_string = json.loads(req[0])
+    count = json_string['CountPage']
+    page = json_string['NumberPage']
     if req[0] == 'unseen':
-         await callback.message.delete()
+        markup = types.InlineKeyboardMarkup()
+        markup.add(types.InlineKeyboardButton(text='Записаться', callback_data='confirm_on_event'))
+        text = f"Вы хотите записаться на:\n {i[page]['name']}\n\n"
+        f"Целевая аудитория: {i[page]['targetGroup']['name']}\n\n"
+        f"Тип курса: {i[page]['type']['name']}\n\n"
+        f"Почта: {i[page]['speakerEmail']}\n\n"
+        f"Телефон для связи: {i[page]['speakerPhone']}\n\n"
+        f"Начало: {i[page]['begin'][3]}:{i[page]['begin'][4]}  Дата:{i[page]['begin'][2]}.{i[page]['begin'][1]}.{i[page]['begin'][0]}"
+        "\n‼️Внимание, при записи на курс, вы даёте согласие на обработку персональных данных, которые были записаны при регистрации"
+        await callback.message.edit_text(text=text, reply_markup=markup)
+
 # Обработка кнопок - вперед и назад
     elif 'pagination' in req[0]:
-        i = categories['content']
 
-        # Расспарсим полученный JSON
-        json_string = json.loads(req[0])
-        count = json_string['CountPage']
-        page = json_string['NumberPage']
-        fakeCount= count + 1
-        fakePage = page + 1
         # Пересоздаем markup
         markup = types.InlineKeyboardMarkup()
-        markup.add(types.InlineKeyboardButton(text='Скрыть', callback_data='unseen'))
+        markup.add(types.InlineKeyboardButton(text='Записаться', callback_data='unseen'))
         # markup для первой страницы
 
         #photo = open('C:/Users/12/PycharmProjects/Telegram_Bot_Copp_Aiogran/photos/ej.jpg', 'rb')
@@ -123,5 +132,8 @@ async def pagination_func(callback: types.CallbackQuery, n=-1):
                                              f"Начало: {i[page]['begin'][3]}:{i[page]['begin'][4]}  Дата:{i[page]['begin'][2]}.{i[page]['begin'][1]}.{i[page]['begin'][0]}",
                                               )
                                               ,reply_markup=markup)
-        #await callback.message.edit_text("Наши мероприятия:\n" + " ", reply_markup=markup)
-#Для пагнинации
+
+
+@dp.callback_query_handler(lambda call: call.data.startswith('confirm_on_events'))
+async def confirm_on_event_func(callback: types.CallbackQuery):
+    await callback.message.answer("Вы записаны!")
